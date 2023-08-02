c533b35fe37bc1f392c9e43a8891c07793ea1aca
Subhashini <worthyweapon@gmail.com>
2023-08-03 00:17:10 +0530

    getText() vs getAttribute()

---
+++ src/main/java/practice/GetText_vs_GetAttribute.java
@@ -0,0 +1,7 @@
+package practice;
+
+/* Get Text() = It is used when the visible text is like this "<label for="name" class="label">Enter your full Name</label>"
+ * 
+ * Get Attribute() = It is used when the visible text has an attribute assigned to it "<input id="fullName" type="text" placeholder="Enter first & last name" class="input is-focused">".
+ * 						Here, placeholder is the attribute
+ */
