f56397dd7ce3d1ff37b1ab028865fc0999650675
Subhashini <worthyweapon@gmail.com>
2023-08-03 00:28:44 +0530

    close() vs quit()

---
+++ src/main/java/practice/Close_vs_Quit.java
@@ -0,0 +1,12 @@
+package practice;
+
+/*
+ * close() = The browser will get closed but it will be active in the memory. 
+ * 					Can be checked in Task manager(chromedriver.exe will be running in task manager
+ * 			 If 2 tabs are open, close() will close only the current tab
+ * 
+ * quit() = Closes the browser and leaves the memory. 
+ * 
+ * 			If 2 tabs are open, quit() will close the browser. It will be used 
+ * every time unless there is an use for close()	
+ */
